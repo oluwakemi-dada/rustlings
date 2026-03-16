e5f48669b3a943a8b27048dc6cf1c74156ac73e4
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-16 08:49:32 +0100

    solve 03_if/if2

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,13 +2,24 @@
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
+    } else if food == "broccoli" {
+        "No thanks!"
+    } else if food == "gummy bears" {
+        "No thanks!"
     } else {
-        1
+        "No thanks!"
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}", picky_eater("strawberry"));
+    println!("{}", picky_eater("potato"));
+    println!("{}", picky_eater("broccoli"));
+    println!("{}", picky_eater("gummy bears"));
+    println!("{}", picky_eater("bread"));
 }
 
 // TODO: Read the tests to understand the desired behavior.
