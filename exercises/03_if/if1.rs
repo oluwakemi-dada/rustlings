f0474893b42dd1317583f0f6d933cbdbb96c494a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-16 08:37:06 +0100

    solve 03_if/if1

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,18 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        a
+    } else if b > a {
+        b
+    } else {
+        a | b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}", bigger(12, 15));
 }
 
 // Don't mind this for now :)
